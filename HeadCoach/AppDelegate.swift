9c2ab5c11370feef20eff86a61e597a9d19c66f2
Ian Malerich <ian.malerich@gmail.com>
2016-01-30 00:52:28 -0600

    access to view controllers from the root view controller for experimenting on

--- HeadCoach/AppDelegate.swift
+++ HeadCoach/AppDelegate.swift
@@ -15,8 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // we will not be using any storyboards, so the following sets up the primary (and only window)
+        let nav = UINavigationController(rootViewController: HCRootViewController())
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        window?.rootViewController = HCRootViewController() // sets the root view controller for the application
+        window?.rootViewController = nav // sets the root view controller for the application
         window?.makeKeyAndVisible()
 
         return true
--- HeadCoach/HCRootViewController.swift
+++ HeadCoach/HCRootViewController.swift
@@ -9,12 +9,65 @@
 import UIKit
 import SnapKit
 
-class HCRootViewController: UIViewController {
+class HCRootViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // make super dope app here
+        view.addSubview(self.tableView)
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "BasicCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.snp_makeConstraints(closure: { make in
+            make.edges.equalTo(self.view)
+        })
+
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BasicCell")
+        cell?.textLabel?.text = ["Live Game", "Landing View", "Team Management", "Drafting View", "Player Detail", "User Detail"][indexPath.row];
+
+        return cell!
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        var vc: UIViewController?
+        switch (indexPath.row) {
+        case 0:
+            vc = HCLiveGameViewController()
+            break;
+        case 1:
+            vc = HCLandingViewController()
+            break;
+        case 2:
+            vc = HCTeamManagementViewController()
+            break;
+        case 3:
+            vc = HCDraftingViewController()
+            break;
+        case 4:
+            vc = HCPlayerDetailViewController()
+            break;
+        case 5:
+            vc = HCUserDetailViewController()
+            break;
+        default:
+            break;
+        }
+
+        self.navigationController?.pushViewController(vc!, animated: true)
     }
 
 }
