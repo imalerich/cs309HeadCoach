c9b322ca3940f2e8a70622b1b1721de9ad3bd21a
Ian Malerich <ian.malerich@gmail.com>
2016-02-08 16:08:13 -0600

    experiments with PHP, generating prime numbers and writing them to a json file

--- Sites/index.php
+++ Sites/index.php
@@ -1,25 +1,44 @@
-<html>
-	<head>
-		<title>PHP Test</title>
-	</head>
+<?php
 
-	<body>
-		<?php 
-		switch(2) {
-		case 0:
-			echo '0';
-			break;
-		case 1:
-		case 2:
-		case 3:
-			echo '1, 2, or 3';
-			break;
-		case 4:
-			echo '4';
-			break;
-		default:
-			break;
+header('Content-Type: application/json');
+
+class SampleAPI {
+	// main method for the sample api
+	function sample($input) {
+		$arr = array($input, $input, $input);
+		echo json_encode($arr, 0, sizeof($arr));
+	}
+}
+
+echo "[";
+
+// create an instance of SampleAPI and call sample
+if (sizeof($_GET) > 0) {
+	echo ",";
+	$api = new SampleAPI;
+	$api->sample($_GET["param"]);
+}
+
+// calculate some prime numbers and output them
+$count = 100;
+$prime = array();
+
+for ($i = 3; $i < $count; $i++) {
+	$isPrime = True;
+
+	for ($j = 2; $j <= sqrt($i); $j++) {
+		if ($i % $j == 0) {
+			$isPrime = False;
 		}
-		?>
-	</body>
-</html>
+	}
+
+	if ($isPrime) {
+		$prime[sizeof($prime)] = $i;
+	}
+}
+
+echo json_encode($prime, 0, sizeof($prime));
+
+echo "]";
+
+?>
