bffb0f1db7695b1266ff2d03fc6c1f630589a6f6
Ian Malerich <ian.malerich@gmail.com>
2016-02-07 21:47:18 -0600

    messed around with PHP a little bit

--- Sites/index.php
+++ Sites/index.php
@@ -4,6 +4,22 @@
 	</head>
 
 	<body>
-		<?php echo '<p>Hello World!</p>'; ?>
+		<?php 
+		switch(2) {
+		case 0:
+			echo '0';
+			break;
+		case 1:
+		case 2:
+		case 3:
+			echo '1, 2, or 3';
+			break;
+		case 4:
+			echo '4';
+			break;
+		default:
+			break;
+		}
+		?>
 	</body>
 </html>
