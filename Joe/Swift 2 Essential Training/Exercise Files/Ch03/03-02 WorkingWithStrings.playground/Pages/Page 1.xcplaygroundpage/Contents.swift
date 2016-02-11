324096957d798ebe76fd2fee6610cfc743d8bf1a
Joseph Young <jayoung@iastate.edu>
2016-02-11 11:55:40 -0600

    Continued Swift Experiments

--- Joe/Swift 2 Essential Training/Exercise Files/Ch03/03-02 WorkingWithStrings.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
+++ Joe/Swift 2 Essential Training/Exercise Files/Ch03/03-02 WorkingWithStrings.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -14,6 +14,13 @@ let area = M_PI * radius * radius
 
 print("The area of a \(shape) with a radius of \(radius) is \(area).")
 
+let numDays = 5
 
+let numString = "\(numDays)"
 
+let numString2 = String(numDays)
+
+let toPrint = "There are \(numDays) days until the weekend"
+
+var myArray : [Int] = [3,5,7]
 //: [Next](@next)
