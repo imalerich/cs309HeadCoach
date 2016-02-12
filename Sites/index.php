fc691aaf049b12baad592fc6f8350f8dfbeb29d2
Ian Malerich <ian.malerich@gmail.com>
2016-02-12 11:32:56 -0600

    pulling all football player and displaying them on a web page

--- Sites/index.php
+++ Sites/index.php
@@ -1,22 +1,40 @@
-<?php
+<html>
 
-header('Content-Type: application/json');
+<head>
+	<title>NFL Players</title>
+</head>
 
-require_once 'HTTP/Request2.php';
+<body>
 
-$request = new HTTP_Request2("https://api.fantasydata.net/nfl/v2/JSON/BoxScores/2015/1");
-$url = $request->getUrl();
+	<?php
+	require_once 'HTTP/Request2.php';
+	
+	$request = new HTTP_Request2("https://api.fantasydata.net/nfl/v2/JSON/Players");
+	$url = $request->getUrl();
+	
+	$headers = array("Ocp-Apim-Subscription-Key" => "fa953b83a78d44a1b054b0afbbdff57e");
+	$request->setHeader($headers);
+	$request->setMethod(HTTP_Request2::METHOD_GET);
+	$request->setBody("{body}");
+	
+	try {
+		$response = $request->send();
 
-$headers = array("Ocp-Apim-Subscription-Key" => "fa953b83a78d44a1b054b0afbbdff57e");
-$request->setHeader($headers);
-$request->setMethod(HTTP_Request2::METHOD_GET);
-$request->setBody("{body}");
+		$json = json_decode($response->getBody(), True);
+		for ($i = 0; $i < sizeof($json); $i++) {
+			$first = $json[$i]["FirstName"];
+			$last  = $json[$i]["LastName"];
+			echo "<h1>$first $last</h1>";
 
-try {
-	$response = $request->send();
-	echo $response->getBody();
-} catch (HttpException $e) {
-	echo "Request failed with error - $e";
-}
+			$img = $json[$i]["PhotoUrl"];
+			echo "<img src=$img alt=$last/>";
+		}
+	
+	} catch (HttpException $e) {
+		echo "Request failed with error - $e";
+	}
+	
+	?>
 
-?>
+</body>
+</html>
