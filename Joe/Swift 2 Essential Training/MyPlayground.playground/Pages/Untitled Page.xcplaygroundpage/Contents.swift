2eb6809da0700b93d3d8885d68671fa5f30b6712
Joseph Young <jayoung@iastate.edu>
2016-02-11 18:59:38 -0600

    Continued experiments

--- Joe/Swift 2 Essential Training/MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Joe/Swift 2 Essential Training/MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -2,8 +2,50 @@
 
 import UIKit
 
-var str = "Hello, playground"
 
-var a : String = "text example"
+/*
+var complexArray : [Any] = Array()
+
+complexArray.append(5)
+complexArray.append("A String")
+
+complexArray.count
+complexArray.removeAtIndex(0)
+
+
+
+var dictionary1 = ["apple": "red", "banana": "yellow"]
+
+var dictionary2 : [String: Int] = Dictionary()
+dictionary2["Hello"] = 1
+
+var dictionary3 : [String: Any] = Dictionary()
+
+dictionary3["Goodbye"] = "A String"
+dictionary3["Key"] = 0
+*/
+
+
+// practice with optionals
+
+var a : Int?
+a = 3
+var b = a! + 5
+
+// optional binding
+if let nonOptionalValue = a {
+    //perform operation
+    var c = nonOptionalValue + 5
+}
+
+
+import Foundation
+
+var optString : String?
+
+optString = "First "
+
+optString?.stringByAppendingString("more")
+
 
 //: [Next](@next)
