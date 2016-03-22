cf8e7b72d9378958282ad9c4fbf6f3ce2ba3918d
Ian Malerich <ian.malerich@gmail.com>
2016-03-22 09:53:17 -0500

    Testing and completing the addUser API call

--- Sites/leagues/addUser.php
+++ Sites/leagues/addUser.php
@@ -21,6 +21,26 @@ if (!array_key_exists("user", $_GET)) {
 	die("'user' id required");
 }
 
+// first make sure that the user exists
+// we will not allow non existent users to join a league
+$query  = "SELECT * FROM users WHERE id='{$_GET["user"]}'";
+$result = mysqli_query($db, $query);
+
+if ($result) {
+	if (!mysqli_fetch_assoc($result)) {
+		echo json_encode(
+			array(
+				"error" => True,
+				"error_msg" => "USER_DOES_NOT_EXIST"
+			)
+		);
+
+		exit();
+	}
+} else {
+	die("Database query failed with errer: " . mysqli_error($db));
+}
+
 // we need to find the league, and look for the first open
 // position for this user, if there are no open positions,
 // an error will be returned to the caller
@@ -36,15 +56,28 @@ $league = mysqli_fetch_assoc($result);
 mysqli_free_result($result);
 
 // loop through each member and find the first one that is empty
+// we will also check if this user is already a member of the league
+
 $first_empty = -1;
 for ($i = 0; $i < 5; $i++) {
 	$key = "member" . $i;
 	$user = $league[$key];
 
-	if ($user == 0) {
+	if ($user == 0 && $first_empty == -1) {
 		// we found a spot for the user!!!
-		$first_empty = $user;
-		break;
+		$first_empty = $i;
+	}
+
+	if ($user == $_GET["user"]) {
+		// user already exists in the league
+		echo json_encode(
+			array(
+				"error" => True,
+				"error_msg" => "USER_ALREADY_IN_LEAGUE"
+			)
+		);
+
+		exit();
 	}
 }
 
@@ -62,12 +95,18 @@ if ($first_empty < 0) {
 
 // now ready to perform the update to the leagues table
 $query = "UPDATE leagues SET member" . $first_empty . "=" . $_GET["user"] 
-	. "WHERE league=" . $_GET["league"];
+	. " WHERE id=" . $_GET["league"];
 
 $result = mysqli_query($db, $query);
 if (!$result) {
 	die("Database query failed with errer: " . mysqli_error($db));
 }
+
+echo json_encode(
+	array(
+		"error" => False,
+	)
+);
 
 // All done! :)
 mysqli_close($db);
