413a681633fa6e96b326b2ad0a9974ce245050d5
Mitchell Johnson <mdj@iastate.edu>
2016-04-03 17:00:32 -0500

    leagueview work

--- iOS/HeadCoach/HCLeagueTableViewCell.swift
+++ iOS/HeadCoach/HCLeagueTableViewCell.swift
@@ -11,52 +11,33 @@ import SnapKit
 
 class HCLeagueTableViewCell: UITableViewCell
 {
-    var lineView = UIView()
-    var leftBox = UIView()
-    var rightBox = UIView()
-    var leftLabel = UILabel()
-    var rightLabel = UILabel()
+    let userImage = UIImageView()
+    let userName = UILabel()
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = UIColor.whiteColor()
-        lineView.backgroundColor = UIColor.grayColor()
-        self.addSubview(leftBox)
-        self.addSubview(rightBox)
-        self.addSubview(lineView)
-        
-      //  self.bringSubviewToFront(lineView)
-        self.rightBox.addSubview(rightLabel)
-        self.leftBox.addSubview(leftLabel)
-        leftBox.backgroundColor = UIColor.whiteColor()
-        rightBox.backgroundColor = UIColor.whiteColor()
-        self.leftBox.snp_makeConstraints { (make) -> Void in
-            make.left.equalTo(self.snp_left)
-            make.right.equalTo(self.snp_centerX)
-            make.top.equalTo(self.snp_top)
-            make.bottom.equalTo(self.snp_bottom)
+        self.addSubview(userImage)
+        self.addSubview(userName)
+        userImage.layer.cornerRadius = 15
+        userImage.layer.masksToBounds = true
+        userImage.layer.borderColor = UIColor.blackColor().CGColor
+        userImage.layer.borderWidth = 2
+        //TODO add actual images
+        userImage.load("https://yt3.ggpht.com/-9JtIWfELi1A/AAAAAAAAAAI/AAAAAAAAAAA/sY8X2YGMGjU/s900-c-k-no/photo.jpg")
+        userImage.snp_makeConstraints { (make) in
+            make.left.lessThanOrEqualTo(5)
+            make.size.equalTo(CGSizeMake(50, 50))
+            make.top.lessThanOrEqualTo(5)
         }
-        self.rightBox.snp_makeConstraints { (make) -> Void in
-            make.left.equalTo(self.snp_centerX)
-            make.right.equalTo(self.snp_right)
-            make.top.equalTo(self.snp_top)
-            make.bottom.equalTo(self.snp_bottom)
-        }
-        self.rightLabel.snp_makeConstraints { (make) -> Void in
-            make.center.equalTo(rightBox.snp_center)
+        userName.snp_makeConstraints { (make) in
+            make.left.equalTo(userImage.snp_left)
+            make.top.equalTo(userImage.snp_bottom)
             
         }
-        self.leftLabel.snp_makeConstraints { (make) -> Void in
-            make.center.equalTo(leftBox.snp_center)
-        }
-        self.lineView.snp_makeConstraints { (make) -> Void in
-            make.centerX.equalTo(self.snp_centerX)
-            make.top.equalTo(self.snp_top)
-            make.bottom.equalTo(self.snp_bottom)
-            make.width.equalTo(1)
-        }
-        leftLabel.text = "left"
-        rightLabel.text = "right"
+        
+       
+ 
         
     }
 
