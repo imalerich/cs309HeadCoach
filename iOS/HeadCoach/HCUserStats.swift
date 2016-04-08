fee57876d7004403cc01c86449ce1b93f90315c9
Ian Malerich <ian.malerich@gmail.com>
2016-04-07 23:05:48 -0500

    new API call, and some refactoring

---
+++ iOS/HeadCoach/HCUserStats.swift
@@ -0,0 +1,13 @@
+//
+//  HCUserStats.swift
+//  HeadCoach
+//
+//  Created by Ian Malerich on 4/7/16.
+//  Copyright © 2016 Group08. All rights reserved.
+//
+
+import UIKit
+
+class HCUserStats: NSObject {
+
+}
