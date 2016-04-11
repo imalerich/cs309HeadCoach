888173f3ac6f7a841429015642388c3a294aed8a
Mitchell Johnson <mdj@iastate.edu>
2016-04-11 13:37:18 -0500

    more user profile UI

--- iOS/HeadCoach/HCUserDetailViewController.swift
+++ iOS/HeadCoach/HCUserDetailViewController.swift
@@ -18,17 +18,30 @@ class HCUserDetailViewController: UIViewController,I3DragDataSource,UITableViewD
     var active = UITableView()
     let container = UIView()
     let profileImage = UIImageView()
+    let activeLabel = UILabel()
+    let benchLabel = UILabel()
+    let userName = UILabel()
+    let record = UILabel()
+    let position = UILabel()
     var gestureCoordinator = I3GestureCoordinator.init()
     var user = ""
 
-    var testarray:NSMutableArray = ["a","b"]
-    var testarray2:NSMutableArray = ["c"]
+    var testarray:NSMutableArray = []
+    var testarray2:NSMutableArray = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.automaticallyAdjustsScrollViewInsets = false
         self.view.addSubview(container)
         self.view.addSubview(profileImage)
+        self.container.addSubview(activeLabel)
+        self.container.addSubview(benchLabel)
+        self.view.addSubview(userName)
+        self.view.addSubview(record)
+        self.view.addSubview(position)
+        userName.text = HCHeadCoachDataProvider.sharedInstance.user!.name
+        benchLabel.text = "Bench"
+        activeLabel.text = "Active"
         view.backgroundColor = UIColor.whiteColor()
         bench.backgroundColor = UIColor(white: 0.0, alpha: 0.1)
         bench.layer.borderColor = UIColor.darkGrayColor().CGColor
@@ -51,18 +64,16 @@ class HCUserDetailViewController: UIViewController,I3DragDataSource,UITableViewD
             make.height.equalTo(100)
             make.width.equalTo(100)
         }
-        
         container.snp_makeConstraints { (make) in
-            make.height.equalTo(350)
+            make.top.equalTo(self.view.snp_centerY).multipliedBy(0.55)
             make.left.right.equalTo(self.view)
             make.bottom.equalTo(self.view.snp_bottom)
             make.centerX.equalTo(self.view.snp_centerX)
-        
         }
         
         self.container.addSubview(active)
         active.snp_makeConstraints { (make) in
-            make.height.equalTo(345)
+            make.top.equalTo(self.container.snp_top).inset(30)
             make.left.equalTo(self.container).inset(5)
             make.bottom.equalTo(self.container).inset(5)
             make.width.equalTo(self.container).multipliedBy(0.5).inset(5)
@@ -72,16 +83,37 @@ class HCUserDetailViewController: UIViewController,I3DragDataSource,UITableViewD
         
         self.container.addSubview(bench)
         bench.snp_makeConstraints { (make) in
-            make.height.equalTo(345)
+            make.top.equalTo(self.container.snp_top).inset(30)
             make.right.equalTo(self.container).inset(5)
             make.bottom.equalTo(self.container).inset(5)
             make.width.equalTo(self.container).multipliedBy(0.5).inset(5)
             //            make.height.equalTo(self.view)
             //            make.width.equalTo(self.view).multipliedBy(0.5)
         }
+        
+        userName.snp_makeConstraints { (make) in
+            make.left.equalTo(self.profileImage.snp_right).inset(-5)
+            make.top.equalTo(self.profileImage.snp_top).inset(5)
+            make.width.equalTo(100)
+        }
+        
+        activeLabel.snp_makeConstraints { (make) in
+            make.top.equalTo(self.container.snp_top)
+            make.centerX.equalTo(self.active.snp_centerX)
+            make.height.equalTo(30)
+            make.width.equalTo(100)
+        }
+        
+        benchLabel.snp_makeConstraints { (make) in
+            make.top.equalTo(self.container.snp_top)
+            make.centerX.equalTo(self.bench.snp_centerX)
+            make.height.equalTo(30)
+            make.width.equalTo(100)
+        }
+        
         gestureCoordinator = I3GestureCoordinator.basicGestureCoordinatorFromViewController(self, withCollections:[self.active,self.bench], withRecognizer: UILongPressGestureRecognizer.init())
-        active.registerClass(HCLeagueCell.classForCoder(), forCellReuseIdentifier: "test1")
-        bench.registerClass(HCLeagueCell.classForCoder(), forCellReuseIdentifier: "test1")
+        active.registerClass(PlayerTableViewCell.classForCoder(), forCellReuseIdentifier: "test1")
+        bench.registerClass(PlayerTableViewCell.classForCoder(), forCellReuseIdentifier: "test1")
         gestureCoordinator.renderDelegate = I3BasicRenderDelegate.init()
         gestureCoordinator.dragDataSource = self
         active.dataSource = self
@@ -97,14 +129,12 @@ class HCUserDetailViewController: UIViewController,I3DragDataSource,UITableViewD
                     self.testarray.addObject(players[i])
                 }
             }
-            
             self.active.reloadData()
             self.bench.reloadData()
         }
     
     }
    
-    
     internal func dataForCollection(collection:UIView) ->NSMutableArray{
         if (collection==self.active){
             return self.testarray
@@ -147,38 +177,40 @@ class HCUserDetailViewController: UIViewController,I3DragDataSource,UITableViewD
         }
     }
     
-    
-    
     func deleteItemAt(at: NSIndexPath!, inCollection collection: UIView!) {
         let fromTable = collection as! UITableView
-        
         let templist = self.dataForCollection(collection)
         print(at.row)
         templist.removeObjectAtIndex(at.row)
         fromTable.deleteRowsAtIndexPaths([NSIndexPath(forRow: at.row,inSection: 0)], withRowAnimation: UITableViewRowAnimation.Fade)
-    
     }
     
     func rearrangeItemAt(from: NSIndexPath!, withItemAt to: NSIndexPath!, inCollection collection: UIView!) {
-        print("rearrangeItemat")
         let targetTableView = collection as! UITableView
-        let dafsdfasdf = self.dataForCollection(collection)
-        dafsdfasdf.exchangeObjectAtIndex(to.row, withObjectAtIndex: from.row)
+        let temp = self.dataForCollection(collection)
+        temp.exchangeObjectAtIndex(to.row, withObjectAtIndex: from.row)
         targetTableView.reloadRowsAtIndexPaths([to,from], withRowAnimation: UITableViewRowAnimation.Fade)
-      
-
     }
     
-    
     func dropItemAt(from: NSIndexPath!, fromCollection: UIView!, toItemAt to: NSIndexPath!, toCollection: UIView!) {
-        print("adfasdfasdfasdfasdfdsf")
         let fromTable = fromCollection as! UITableView
         let toTable = toCollection as! UITableView
+        
+        if(fromTable==self.active){
+            HCHeadCoachDataProvider.sharedInstance.movePlayerToBench(self.testarray[from.row] as! HCPlayer, league: HCHeadCoachDataProvider.sharedInstance.league!, completion: { (error) in
+                print("move to bench")
+                print(error)
+            })
+        }else{
+            HCHeadCoachDataProvider.sharedInstance.movePlayerToActive(self.testarray2[from.row] as! HCPlayer, league: HCHeadCoachDataProvider.sharedInstance.league!, completion: { (error) in
+                print("move to active")
+                print(error)
+            })
+        }
         
         let fromDataset = self.dataForCollection(fromTable) as NSMutableArray
         let toDataset = self.dataForCollection(toTable) as NSMutableArray
         let dropDatum = fromDataset.objectAtIndex(from.row)
-        
         fromDataset.removeObjectAtIndex(from.row)
         toDataset.insertObject(dropDatum, atIndex: to.row)
         
@@ -188,17 +220,23 @@ class HCUserDetailViewController: UIViewController,I3DragDataSource,UITableViewD
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-//        let vc = HCPlayerMoreDetailController()
-//        vc.player = tableView[indexPath.row]
-//        navigationController?.pushViewController(vc, animated: true)
+        let realm = try! Realm()
+        let player = self.dataForCollection(tableView)[indexPath.row] as! HCPlayer
+        let temp = realm.objectForPrimaryKey(FDPlayer.self, key: player.fantasy_id)
+        print(temp?.name)
+        let vc = HCPlayerMoreDetailController()
+        vc.player = temp
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-            let cell = tableView.dequeueReusableCellWithIdentifier("test1", forIndexPath: indexPath) as UITableViewCell
-//            let player = self.dataForCollection(tableView)[indexPath.row] as! HCPlayer
-//            cell.textLabel?.text = player.name
-            cell.textLabel?.text = self.dataForCollection(tableView)[indexPath.row] as! String
+            let cell = tableView.dequeueReusableCellWithIdentifier("test1", forIndexPath: indexPath) as! PlayerTableViewCell
+            let player = self.dataForCollection(tableView)[indexPath.row] as! HCPlayer
+            let realm = try! Realm()
+            let temp = realm.objectForPrimaryKey(FDPlayer.self, key: player.fantasy_id)
+            cell.setPlayer(temp!)
+            cell.playerImage.load((temp?.photoURL)!)
             cell.textLabel?.textColor = UIColor.blackColor()
             cell.separatorInset = UIEdgeInsetsZero
             cell.layoutMargins = UIEdgeInsetsZero
